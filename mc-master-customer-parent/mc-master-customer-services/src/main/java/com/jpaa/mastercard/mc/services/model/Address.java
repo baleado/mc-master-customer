01dd9fb0333e81863af1db4dd092478a4cfbf740
João Anacleto <joao.anacleto@pst.asseco.com>
2022-09-07 22:51:22 +0100

    Add hashCode and equals to Address and Customer classes

--- mc-master-customer-parent/mc-master-customer-services/src/main/java/com/jpaa/mastercard/mc/services/model/Address.java
+++ mc-master-customer-parent/mc-master-customer-services/src/main/java/com/jpaa/mastercard/mc/services/model/Address.java
@@ -84,6 +84,64 @@ public class Address {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((addressType == null) ? 0 : addressType.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((customerId == null) ? 0 : customerId.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (addressType != other.addressType)
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (customerId == null) {
+			if (other.customerId != null)
+				return false;
+		} else if (!customerId.equals(other.customerId))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Address [id=" + id + ", street=" + street + ", state=" + state + ", country=" + country + ", zipCode="
 				+ zipCode + ", customerId=" + customerId + ", addressType=" + addressType + "]";
